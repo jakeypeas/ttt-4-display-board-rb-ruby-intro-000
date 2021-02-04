4450d2f925d5e04c627335f2e17edf47cb009a51
Jacob Pearce <jkjake1234@gmail.com>
2021-02-04 17:49:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,13 +5,15 @@ def display_board(board)
   puts rows[2]="   |   |   "
   puts rows[3]="-----------"
   puts rows[4]="   |   |   "
-
+end
+def display_board(board)
     board = [" "," "," "," ","X"," "," "," "," "]
       puts rows[0]="   |   |   "
     puts rows[1]= "-----------"
     puts rows[2]="   | #{board[5]} |   "
     puts rows[3]="-----------"
     puts rows[4]="   |   |   "
+end
 
       board = ["O"," "," "," "," "," "," "," "," "]
       puts board[0]="O"
