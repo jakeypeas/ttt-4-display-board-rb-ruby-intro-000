5ebd4f621375dd5dc511399e3531be7a6f5514dc
Jacob Pearce <jkjake1234@gmail.com>
2021-02-04 16:54:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+display_board(board)
+(rows[0])=("   |   |   ")
+(rows[1])=("-----------")
+(rows[2])=("   |   |   ")
+(rows[3])=("-----------")
+(rows[4])=("   |   |   ")
