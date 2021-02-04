7e79029f751d8668a0ebe4320cde4a58d2001bc8
Jacob Pearce <jkjake1234@gmail.com>
2021-02-04 17:24:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,3 +16,57 @@ def display_board(board)
     puts rows[3]="-----------"
     puts rows[4]="   |   |   "
     end
+    def display_board(board)
+      board = ["O"," "," "," "," "," "," "," "," "]
+      rows = [" ", " ", " "]
+        puts rows[0]=" O |   |   "
+      puts rows[1]= "-----------"
+      puts rows[2]="   |   |   "
+      puts rows[3]="-----------"
+      puts rows[4]="   |   |   "
+      end
+      def display_board(board)
+        board = ["O"," "," "," ","X"," "," "," "," "]
+        rows = [" ", " ", " "]
+          puts rows[0]=" O |   |   "
+        puts rows[1]= "-----------"
+        puts rows[2]="   | X |   "
+        puts rows[3]="-----------"
+        puts rows[4]="   |   |   "
+        end
+        def display_board(board)
+          board = ["X","X","X"," "," "," "," "," "," "]
+          rows = [" ", " ", " "]
+            puts rows[0]=" X | X | X "
+          puts rows[1]= "-----------"
+          puts rows[2]="   |   |   "
+          puts rows[3]="-----------"
+          puts rows[4]="   |   |   "
+          end
+          def display_board(board)
+            board = [" "," "," "," "," "," ","O","O","O"]
+            rows = [" ", " ", " "]
+              puts rows[0]="   |   |   "
+            puts rows[1]= "-----------"
+            puts rows[2]="   |   |   "
+            puts rows[3]="-----------"
+            puts rows[4]=" O | O | O "
+            end
+            def display_board(board)
+              board = ["X"," "," "," ","X"," "," "," ","X"]
+              rows = [" ", " ", " "]
+                puts rows[0]=" X |   |   "
+              puts rows[1]= "-----------"
+              puts rows[2]="   | X |   "
+              puts rows[3]="-----------"
+              puts rows[4]="   |   | X "
+              end
+              def display_board(board)
+                board = [" "," ","O"," ","O"," ","O"," "," "]
+                rows = [" ", " ", " "]
+                  puts rows[0]="   |   | O "
+                puts rows[1]= "-----------"
+                puts rows[2]="   | O |   "
+                puts rows[3]="-----------"
+                puts rows[4]=" O |   |   "
+                end
