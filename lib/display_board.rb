f6be8963a27c63ff6a40ef9f00947236a36f2f74
Jacob Pearce <jkjake1234@gmail.com>
2021-02-04 17:51:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,8 +13,8 @@ def display_board(board)
     puts rows[2]="   | #{board[5]} |   "
     puts rows[3]="-----------"
     puts rows[4]="   |   |   "
-end
-
+  end
+def display_board(board)
       board = ["O"," "," "," "," "," "," "," "," "]
       puts board[0]="O"
         puts rows[0]=" O |   |   "
@@ -22,40 +22,44 @@ end
       puts rows[2]="   |   |   "
       puts rows[3]="-----------"
       puts rows[4]="   |   |   "
-
+    end
+    def display_board(board)
         board = ["O"," "," "," ","X"," "," "," "," "]
           puts rows[0]=" O |   |   "
         puts rows[1]= "-----------"
         puts rows[2]="   | X |   "
         puts rows[3]="-----------"
         puts rows[4]="   |   |   "
-
+      end
+      def display_board(board)
           board = ["X","X","X"," "," "," "," "," "," "]
             puts rows[0]=" X | X | X "
           puts rows[1]= "-----------"
           puts rows[2]="   |   |   "
           puts rows[3]="-----------"
           puts rows[4]="   |   |   "
-
+        end
+        def display_board(board)
             board = [" "," "," "," "," "," ","O","O","O"]
               puts rows[0]="   |   |   "
             puts rows[1]= "-----------"
             puts rows[2]="   |   |   "
             puts rows[3]="-----------"
             puts rows[4]=" O | O | O "
-
-  board = ["X"," "," "," ","X"," "," "," ","X"]
-    puts rows[0]=" X |   |   "
-    puts rows[1]= "-----------"
-    puts rows[2]="   | X |   "
-    puts rows[3]="-----------"
-    puts rows[4]="   |   | X "
-
-
-  board = [" "," ","O"," ","O"," ","O"," "," "]
-    puts rows[0]="   |   | O "
-    puts rows[1]= "-----------"
-    puts rows[2]="   | O |   "
-    puts rows[3]="-----------"
-    puts rows[4]=" O |   |   "
-    end
+          end
+          def display_board(board)
+              board = ["X"," "," "," ","X"," "," "," ","X"]
+              puts rows[0]=" X |   |   "
+              puts rows[1]= "-----------"
+              puts rows[2]="   | X |   "
+              puts rows[3]="-----------"
+              puts rows[4]="   |   | X "
+            end
+            def display_board(board)
+              board = [" "," ","O"," ","O"," ","O"," "," "]
+              puts rows[0]="   |   | O "
+              puts rows[1]= "-----------"
+              puts rows[2]="   | O |   "
+              puts rows[3]="-----------"
+              puts rows[4]=" O |   |   "
+            end
